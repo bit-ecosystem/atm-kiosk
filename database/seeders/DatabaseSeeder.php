df4332d21c30dbe2d00c2237c21da64a8a724425
a-faros <faros@email.com>
2025-02-18 13:37:14 +0800

    ew

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,16 +15,5 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call([
             JsonDBSeeder::class, ]);
-
-        User::factory()->create([
-            'name' => 'Faros',
-            'email' => 'admin@email.com',
-            'user_type_id' => 12,
-        ]);
-        User::factory()->create([
-            'name' => 'Ahmad Faros',
-            'email' => 'ahmadfaros.othman@amkor.com',
-            'user_type_id' => 10,
-        ]);
     }
 }
